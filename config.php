d681ef6f5d778018d6087477d264af1612b5fe17
zBenjx <80025123+zBenjx@users.noreply.github.com>
2021-03-04 05:06:22 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+
+<!--
+_       __                              __
+| |     / /___  ____  ____  ____ _____ _/ /_
+| | /| / / __ \/ __ \/ __ \/ __ `/ __ `/ __ \
+| |/ |/ / /_/ / /_/ / /_/ / /_/ / /_/ / / / /
+|__/|__/\____/\____/ .___/\__,_/\__,_/_/ /_/
+                 /_/
+
+Made by Tomás Guerra ® http://www.tomasguerra.cl to MC-Market Community
+
+All rights reserved ® 2019
+                                                                                -->
+
+<?php
+
+$siteTitle = "MC-Market Minecraft Server";
+$favicon = "https://gamepedia.cursecdn.com/minecraft_gamepedia/4/44/Grass_Block_Revision_6.png";
+$backgroundImage = "https://i.imgur.com/xbfQ5QQ.jpg";
+$logoImage = "https://gamepedia.cursecdn.com/minecraft_gamepedia/4/44/Grass_Block_Revision_6.png";
+$mainLogo = "https://visage.surgeplay.com/bust/99cb3d7ff8e64a0da6a2d4736d11a19b";
+$linkHome = "#";
+$linkPunishments = "/punishments";
+$linkStore = "/store";
+$linkTwitter = "/twitter";
+$linkDiscord = "/discord";
+$serverName = "MC-Market";
+$serverIp = "mc.hypixel.net";
+$serverCopyright = "MC-Market";
+ ?>

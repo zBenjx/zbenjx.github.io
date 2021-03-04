ffccbb4bef2a2b3b9967a8f504f440c09237670a
zBenjx <80025123+zBenjx@users.noreply.github.com>
2021-03-04 16:57:08 -0300

    Update config.php

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ Made by Tomás Guerra ® http://www.tomasguerra.cl to MC-Market Community
 All rights reserved ® 2019
                                                                                 -->
 
-<?php
+<?html
 
 $siteTitle = "MC-Market Minecraft Server";
 $favicon = "https://gamepedia.cursecdn.com/minecraft_gamepedia/4/44/Grass_Block_Revision_6.png";
